a26d6caf9eddbb0a0a567154280f2a102fd76183
Denis Kuimov <deniskuimov@mail.ru>
2022-03-28 20:10:51 +0300

    add validation error empty field

--- authRouter.js
+++ authRouter.js
@@ -3,7 +3,13 @@ const router = new Router()
 const controller = require('./authController')
 const {check} = require('express-validator')
 
-router.post('/registration', controller.registration)
+router.post('/registration', [
+    check('username', 'Имя не может быть пустым').notEmpty(),
+    check('password', 'Пароль не может меньше 4 и больше 10 символов ').isLength({
+        min: 4,
+        max: 10
+    }),
+], controller.registration)
 router.post('/login', controller.login)
 router.get('/users', controller.getUsers)
 
