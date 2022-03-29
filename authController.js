793e1a3b27e3061eb55941f34ae7e96943b549d3
Denis Kuimov <deniskuimov@mail.ru>
2022-03-29 19:49:07 +0300

    add token

--- authController.js
+++ authController.js
@@ -2,6 +2,15 @@ const User =require("./models/User")
 const Role =require("./models/Role")
 const bcrypt = require('bcryptjs')
 const {validationResult } = require('express-validator')
+const jwt = require('jsonwebtoken')
+const { secret } = require("./config")
+
+const generateAccessToken = (id, roles) => {
+    const payload = {
+        id, roles
+    }
+    return jwt.sign(payload, secret, { expiresIn: '24h'})
+}
 
 class authController {
     async registration(req, res) {
@@ -31,7 +40,18 @@ class authController {
     }
     async login(req, res) {
         try {
-
+            const {username, password} = req.body
+            const user = await User.findOne({username})
+            if(!user) {
+                return res.status(400).json({message: `Пользователь ${username} не зарегистрирован`})
+            }
+            const validPassword = bcrypt.compareSync(password, user.password)
+            if(!validPassword) {
+                return res.status(400).json({message: "Неверный пароль"})
+            }
+            
+            const token = generateAccessToken(user._id, user.roles)
+            return res.json({token})
         } catch (e) {
             console.log(e);
             res.status(400).json({message: 'Login error'})
