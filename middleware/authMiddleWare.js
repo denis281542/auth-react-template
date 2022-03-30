60b03f5c0c5616971d3dd2c991b33db65d632bfc
Denis Kuimov <deniskuimov@mail.ru>
2022-03-30 20:43:26 +0300

    add decode toket

--- middleware/authMiddleWare.js
+++ middleware/authMiddleWare.js
@@ -1,10 +1,19 @@
+const jwt = require('jwt')
+const { secret } = require('../config')
+
 module.exports = (req, res, next) => {
     if(req.method === "OPTIONS") {
         next()
     }
 
     try {
-
+        const toket = req.headers.authorization.split(" ")[1]
+        if(!toket) {
+            return res.status(400).json({message: "Пользователь не авторизован"})
+        }
+        const decodedData = jwt.verify(toket, secret)
+        req.user = decodedData
+        next()
     } catch (e) {
         console.log(e)
         return res.status(400).json({message: `Ошибка: ${e}`})
